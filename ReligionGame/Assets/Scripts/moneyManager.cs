b800ea41c7a0acf808a1d39beb887228f82a2a34
steinfernandez <steinfernandez@gmail.com>
2017-01-11 23:23:27 -0500

    Added money UI.

--- ReligionGame/Assets/Scripts/moneyManager.cs
+++ ReligionGame/Assets/Scripts/moneyManager.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class moneyManager : MonoBehaviour {
 
+    int playerMoney = 0;
+    [SerializeField]
+    GameObject moneyUI;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        moneyUI.GetComponent<Text>().text = "0";
 	}
 	
 	// Update is called once per frame
